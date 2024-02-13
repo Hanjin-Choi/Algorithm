4004d34c290cfe44346194aaa94024b489b95a83
halfoffaceeyes <156498561+halfoffaceeyes@users.noreply.github.com>
2024-02-14 00:20:00 +0900

    [Bronze I] Title: 색종이, Time: 1720 ms, Memory: 38300 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,18 +1,15 @@
-import sys
-input = sys.stdin.readline
+N = int(input())
+res = [0] * (N+1)
+board = [[0] * 1001 for _ in range(1001)]
 
-n=int(input())
-arr = [[0]*1001 for _ in range(1001)] #빈어레이
-ans=[0]*(n+1) # 답저장할 리스트
+for num in range(1, N+1):
+    x, y, dx, dy = map(int, input().split())
 
-for i in range(1,n+1): # 순회
-    r,c,w,h = map(int,input().split())
-    for row in range(r,r+w): # 윗장면적
-        for col in range(c,c+h):
-            arr[row][col]=i
+    for i in range(x, x+dx):
+        board[i][y:y+dy] = [num] * dy
 
-for i in range(1,n+1):
-    for row in range(1001):
-        ans[i]+=arr[row].count(i)
-print(*ans[1:],sep='\n')
+for r in board:
+    for num in range(1, N+1):
+        res[num] += r.count(num)
 
+print(*res[1:], sep='\n')
