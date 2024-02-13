14d810192dae700b00aa81e2b7ed4a03c77e52de
halfoffaceeyes <156498561+halfoffaceeyes@users.noreply.github.com>
2024-02-13 22:17:59 +0900

    [Silver IV] Title: 카드2, Time: 196 ms, Memory: 53980 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,25 +1,7 @@
-import sys
-input = sys.stdin.readline
-#
-n = int(input())
-l= [2*x for x in range(1,n//2+1)]
-if n==1:
-    print(1)
-else:
-    if n %2 ==0:
-        discard = n//2
-        motion = 0
-    else:
-        discard = n//2+1
-        motion=1
-    idx = 0
-    while discard < n-1:
-        if l[idx]:
-            if motion % 2 ==0:
-                l[idx]=0
-                discard +=1
-            motion +=1
-        idx+=1
-        if idx ==n//2:
-            idx =0
-    print(max(l))
+import _collections
+L =[x for x in range(1,int(input())+1)]
+dq=_collections.deque(L)
+while len(dq)>1:
+    dq.popleft()
+    dq.rotate(-1)
+print(dq[0])
