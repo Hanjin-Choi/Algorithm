977024c31954e98c4ac1fee282b7faf685dfc773
halfoffaceeyes <156498561+halfoffaceeyes@users.noreply.github.com>
2024-02-14 00:15:00 +0900

    [Bronze I] Title: 색종이, Time: 168 ms, Memory: 38304 KB, Score: 53 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -2,16 +2,17 @@ import sys
 input = sys.stdin.readline
 
 n=int(input())
-arr = [list(map(int,input().split())) for _ in range(n)]
-ans=[0]*n # 답저장할 리스트
-set_area = set() # 면적 저장할 세트
-for i in range(n-1,-1,-1): # 뒤로 순회
-    r,c,w,h = arr[i]
-    comp_set = set()
+arr = [[0]*1001 for _ in range(1001)] #빈어레이
+ans=[0]*(n+1) # 답저장할 리스트
+
+for i in range(1,n+1): # 순회
+    r,c,w,h = map(int,input().split())
     for row in range(r,r+w): # 윗장면적
         for col in range(c,c+h):
-            comp_set.add((row,col))
-    ans[i]=len(comp_set-set_area) # 윗장 면적을 빼준 면적
-    set_area=comp_set|set_area # 면적 저장
-print(*ans,sep='\n')
+            arr[row][col]=i
+
+for i in range(1,n+1):
+    for row in range(1001):
+        ans[i]+=arr[row].count(i)
+print(*ans[1:],sep='\n')
 
