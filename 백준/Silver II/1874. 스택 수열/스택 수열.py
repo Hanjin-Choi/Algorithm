24145623e6b24681ae52dd83d296f20b04ba52a4
halfoffaceeyes <156498561+halfoffaceeyes@users.noreply.github.com>
2024-02-14 20:48:01 +0900

    [Silver II] Title: 스택 수열, Time: 112 ms, Memory: 33488 KB -BaekjoonHub

---
+++ 백준/Silver II/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+stack = []
+count = 1
+result = []
+
+for i in range(1, n + 1):
+    num = int(input())
+    while count <= num:
+        stack.append(count)
+        count += 1
+        result.append("+")
+    if stack[-1] == num:
+        stack.pop()
+        result.append("-")
+    else:
+        print("NO")
+        exit(0)
+
+print("\n".join(result))
