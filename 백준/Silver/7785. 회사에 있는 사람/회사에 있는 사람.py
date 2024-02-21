94c55dba8d8785287b3cca0803afbee27df593d5
halfoffaceeyes <156498561+halfoffaceeyes@users.noreply.github.com>
2024-02-21 09:22:24 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 144 ms, Memory: 42388 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -1,21 +1,14 @@
 import sys
 input = sys.stdin.readline
-
 n=int(input())
-e= {}
-l = {}
+nd=set()
 for i in range(n):
     name,status = input().split()
-    e.setdefault(name,0)
-    l.setdefault(name,0)
     if status=='enter':
-        e[name] +=1
+        nd.add(name)
     else:
-        l[name] +=1
-l_name = []
-for n in e:
-    if e[n]-l[n]>0:
-        l_name.append(n)
-l_name.sort(reverse=True)
-ans = '\n'.join(l_name)
+        nd.remove(name)
+nd = list(nd)
+nd.sort(reverse=True)
+ans = '\n'.join(nd)
 print(ans)
