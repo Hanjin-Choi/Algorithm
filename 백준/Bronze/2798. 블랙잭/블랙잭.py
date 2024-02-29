225b66bfefe12d43f99c4599e4697e8cda381e7a
HanJin Choi <156498561+halfoffaceeyes@users.noreply.github.com>
2024-02-29 10:51:51 +0900

    [Bronze II] Title: 블랙잭, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,9 +1,16 @@
 N, M = map(int,input().split())
 L = list(map(int,input().split()))
+L.sort()
 S = []
-for i in range(N-2):
-    for j in range(i+1, N-1):
-        for k in range(j+1,N):
-            if L[i]+L[j]+L[k]<=M:
-                S.append(L[i]+L[j]+L[k])
-print(max(S))
+def bruteforce():
+    max_sum=0
+    for i in range(N-2):# 전체 순회해서 최대인 값을 출력하기
+        for j in range(i+1, N-1):
+            for k in range(j+1,N):
+                ssum = L[i] + L[j] + L[k]
+                if max_sum < ssum<=M:
+                    max_sum =ssum
+                elif ssum>M:
+                    break
+    return max_sum
+print(bruteforce())
