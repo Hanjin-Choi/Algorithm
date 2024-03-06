d5b30d2d905532135a49d1edc2efeda39c07add1
HanJin Choi <156498561+halfoffaceeyes@users.noreply.github.com>
2024-03-07 00:35:42 +0900

    [Gold I] Title: 이항 계수 3, Time: 468 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11401. 이항 계수 3/이항 계수 3.py
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.py
@@ -1,26 +1,20 @@
-def mul(a,i):
-    if i==1:
-        return a
+def fact(n):
+    f=1
+    for i in range(2,n+1):
+        f =(f*i)%1000000007
+    return f
+def modd(num,i):
+    if i ==1:
+        return num
     if i%2:
-        res = mul(a,i//2)**2*a
-        if res>=1000000007:
-            return res%1000000007
-        else:
-            return res
+        return (modd(num,i//2)**2*num) % 1000000007
     else:
-        res = mul(a,i//2)**2
-        if res >= 1000000007:
-            return res % 1000000007
-        else:
-            return res
-
+        return (modd(num,i//2)**2) % 1000000007
 n,k = map(int,input().split())
-if k < n-k:
+if k<n-k:
     k=n-k
-ans =1
+ja = 1
+mo=fact(n-k)
 for i in range(n-k):
-    ans *= ((n-i)*mul(i+1,1000000005))
-    ans%=1000000007
-
-print(ans)
-
+    ja=(ja*(n-i))%1000000007
+print((modd(mo,1000000005)*ja)%1000000007)
