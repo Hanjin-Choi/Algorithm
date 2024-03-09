27eb74f5d3f62fa3e0b1d622eb23bf59cd7104b2
HanJin Choi <156498561+halfoffaceeyes@users.noreply.github.com>
2024-03-09 11:31:13 +0900

    [Silver II] Title: 마인크래프트, Time: 140 ms, Memory: 34488 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,27 +1,27 @@
-import sys
+import sys,collections
 input = sys.stdin.readline
 
 n,m,b = map(int,input().strip().split())
-ground =[list(map(int,input().split())) for _ in range(n)]
-minn=257
-maxx=0
+ground =[]
 for _ in range(n):
-    minn =min(minn,min(ground[_]))
-    maxx=max(maxx,max(ground[_]))
+    ground+=list(map(int,input().split()))
+minn=min(ground)
+maxx=max(ground)
+
+num = collections.Counter(ground)
+
 time_min=500*500*2*257
 height_max=0
 for height in range(minn,maxx+1):
     t=0
     box =b
-    for row in range(n):
-        for col in range(m):
-            present = ground[row][col]
-            if present>=height:
-                t +=(present-height)*2
-                box +=(present-height)
-            else:
-                t +=(height-present)
-                box -=(height-present)
+    for present, c in num.items():
+        if present>=height:
+            t+=(present-height)*c*2
+            box+=(present-height)*c
+        else:
+            t+=(height-present)*c
+            box-=(height-present)*c
     if box>=0 and time_min>t:
         time_min=t
         height_max=height
