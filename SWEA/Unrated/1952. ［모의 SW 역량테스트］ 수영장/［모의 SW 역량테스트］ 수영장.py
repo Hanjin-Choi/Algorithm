4c3d7f213e01265b58283a928a9b8c1b8b2a8602
HanJin Choi <156498561+halfoffaceeyes@users.noreply.github.com>
2024-03-19 16:38:07 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 311 ms, Memory: 73,396 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
@@ -1,8 +1,7 @@
 t=int(input())
 
-def back(pl,m,cost):
+def back(m,cost):
     global mini
-    temp=pl[:]
     if m==12:
         if mini>cost:
             mini=cost
@@ -10,23 +9,21 @@ def back(pl,m,cost):
         return
     else:
         if plan[m]:
-            back(temp,m+1,cost+price[0]*temp[m])
-            back(temp,m+1,cost+price[1])
-            if m+2<12:
-                temp[m+1]=temp[m+2]=0
-                back(temp,m+1,cost+price[2])
-            elif m+1<12:
-                temp[m+1]=0
-                back(temp,m+1,cost+price[2])
+            back(m+1,cost+price[0]*plan[m])
+            back(m+1,cost+price[1])
+            if m+3<12:
+                back(m+3,cost+price[2])
+            elif m==11:
+                back(12,cost+price[2])
             else:
-                back(temp,m+1,cost+price[2])
+                back(12,cost+price[2])
         else:
-            back(temp,m+1,cost)
+            back(m+1,cost)
 
 
 for tc in range(1,t+1):
     price = list(map(int,input().split()))# 1일 1달 3달 1년
     plan=list(map(int,input().split()))
     mini=price[-1]
-    back(plan,0,0)
+    back(0,0)
     print(f'#{tc} {mini}')
