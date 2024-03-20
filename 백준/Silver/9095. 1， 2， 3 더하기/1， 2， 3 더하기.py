a652765448db8e56628fd18519eb8e733b1101a3
HanJin Choi <156498561+halfoffaceeyes@users.noreply.github.com>
2024-03-21 00:17:06 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+t=int(input())
+tl =[int(input()) for _ in range(t)]
+dp=[0]*12
+ans=[0]*t
+dp[1]=1
+dp[2]=2
+dp[3]=4
+for i in range(4,12):
+    dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
+for i in range(t):
+    ans[i]=dp[tl[i]]
+print(*ans,sep='\n')
