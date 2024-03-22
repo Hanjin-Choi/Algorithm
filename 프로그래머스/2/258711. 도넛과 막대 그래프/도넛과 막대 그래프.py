0074c2993cae16917e72c599c0f6978fb1cbae41
HanJin Choi <156498561+halfoffaceeyes@users.noreply.github.com>
2024-03-22 11:08:31 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -1,39 +1,23 @@
-from collections import deque
-
-def bfs(adjl,s):
-    q=deque([s])
-    visit=[0]*1000001
-    donut=0
-    stick=0
-    eight=0
-    while q:
-        now=q.popleft()
-
-        if not adjl[now]:
-            stick+=1
-        if now!=s and len(adjl[now])==2:
-            eight +=1
-            continue
-        for w in adjl[now]:
-            if visit[w]<=2:
-                if visit[w]==2 and len(adjl[w])==1:
-                    donut +=1
-                else:
-                    q.append(w)
-                    visit[w]+=1
-    return (donut,stick,eight)
 def solution(edges):
     answer = []
     start=set()
     end=set()
-    adjl=[set() for _ in range(1000001)]
+    eight=set()
+    maxx=0
+    for i in edges:
+        maxx=max(maxx,i[0],i[1])
+    print(maxx)
+    adjl=[set() for _ in range(maxx+1)]
     for s,e in edges:
         adjl[s].add(e)
         start.add(s)
         end.add(e)
-    res = start-end
+        if len(adjl[s])==2:
+            eight.add(s)
+    stick=len(end-start)
+    res = eight-end
     for i in res:
-        if len(adjl[i])>1:
-            do,st,egt =bfs(adjl,i)
-            answer=[i,do,st,egt]
+        graph=len(adjl[i])
+        ecount=len(eight-{i})
+        answer=[i,graph-stick-ecount,stick,ecount]
     return answer
