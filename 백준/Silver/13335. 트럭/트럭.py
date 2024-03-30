d998259f61bcf7d8dcf8901b74814dd659d600d2
HanJin Choi <156498561+halfoffaceeyes@users.noreply.github.com>
2024-03-30 11:37:03 +0900

    [Silver I] Title: 트럭, Time: 124 ms, Memory: 34028 KB -BaekjoonHub

---
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -0,0 +1,22 @@
+from collections import deque
+n,w,l =map(int,input().split())
+al=list(map(int,input().split()))
+bridge =deque([0]*w)
+t=0
+idx=0
+cnt=0
+while idx<n:
+    if sum(bridge)+al[idx]<=l:
+        bridge.popleft()
+        bridge.append(al[idx])
+        idx+=1
+        t+=1
+    else:
+        bridge.popleft()
+        if sum(bridge)+al[idx]<=l:
+            bridge.append(al[idx])
+            idx += 1
+        else:
+            bridge.append(0)
+        t+=1
+print(t+w)
