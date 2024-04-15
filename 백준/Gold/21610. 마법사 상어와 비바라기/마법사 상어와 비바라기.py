5cdd30ed0ad57e1ea542bf88d9085283a49d8481
HanJin Choi <156498561+halfoffaceeyes@users.noreply.github.com>
2024-04-15 22:38:30 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 412 ms, Memory: 34264 KB -BaekjoonHub

---
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -0,0 +1,43 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+dr =[0,0,-1,-1,-1,0,1,1,1]
+dc =[0,-1,-1,0,1,1,1,0,-1]
+ddr=[1,1,-1,-1]
+ddc=[1,-1,-1,1]
+def move_cloud(d,s):
+    while cloud:
+        r,c = cloud.popleft()
+        r += dr[d]*s
+        c += dc[d]*s
+        r%=n
+        c%=n
+        arr[r][c]+=1
+        temp_cloud.add((r,c))
+
+def make_cloud():
+    for row in range(n):
+        for col in range(n):
+            if arr[row][col]>=2 and not (row,col) in temp_cloud:
+                arr[row][col]-=2
+                cloud.append((row,col))
+
+n,m = map(int,input().split())
+arr = [list(map(int,input().split())) for _ in range(n)]
+cloud =deque([(n-1,0),(n-1,1),(n-2,0),(n-2,1)])
+for _ in range(m):
+    temp_cloud = set()
+    d,s= map(int,input().split())
+    move_cloud(d,s)
+    for r,c in temp_cloud:
+        for i in range(4):
+            rr= r+ddr[i]
+            cc= c+ddc[i]
+            if 0<=rr<n and 0<=cc<n and arr[rr][cc]:
+                arr[r][c]+=1
+    make_cloud()
+total=0
+for row in range(n):
+    total +=sum(arr[row])
+print(total)
+
