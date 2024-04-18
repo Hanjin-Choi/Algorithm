9a1070603898eccfb12ad517caf7a93cce053eb9
HanJin Choi <156498561+halfoffaceeyes@users.noreply.github.com>
2024-04-18 21:15:51 +0900

    [level 3] Title: 야근 지수, Time: 169.17 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -3,7 +3,7 @@ def solution(n, works):
     answer = 0
     leng =len(works)
     maxx=max(works)
-    dic ={i:0 for i in range(maxx+1)}
+    dic =[0] * (maxx+1)
     for w in works:
         dic[w] +=1 
     for hour in range(n):
@@ -14,6 +14,6 @@ def solution(n, works):
                 maxx -=1
         else:
             break
-    for num in dic:
+    for num in range(maxx+1):
         answer += num**2 * dic[num]  
     return answer
