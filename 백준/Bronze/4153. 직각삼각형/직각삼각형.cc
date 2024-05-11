5119ec4401acbbfbe1ee6563ac4c3da6cc565dc0
HanJin Choi <156498561+halfoffaceeyes@users.noreply.github.com>
2024-05-11 14:50:07 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	while (1) {
+		int x, y, z;
+		cin >> x >> y >> z;
+		if (x == 0 && y == 0 && z == 0) {
+			return 0;
+		}
+		if ((x * x == y * y + z * z) || (y * y == x * x + z * z) || (z * z == x * x + y * y)) {
+					cout << "right" << endl;
+		}
+				else {
+					cout << "wrong" << endl;
+				}
+	}
+}
