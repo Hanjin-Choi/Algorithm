319d0cc3eff7d15090763305ea7b11757f7216c6
HanJin Choi <156498561+halfoffaceeyes@users.noreply.github.com>
2024-06-12 01:38:14 +0900

    [Bronze I] Title: FizzBuzz, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.cc
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.cc
@@ -9,53 +9,27 @@ int main() {
 	string b{};
 	string c{};
 	cin >> a >> b >> c;
+	int last = 0;
 	if (a != "Fizz" && a != "Buzz" && a != "FizzBuzz") {
-		int last = stoi(a) + 3;
-		if (last % 15 == 0) {
-			cout << "FizzBuzz" << "\n";
-		}
-		else if (last % 5 == 0) {
-			cout << "Buzz" << "\n";
-		}
-		else if (last % 3 == 0) {
-			cout << "Fizz" << "\n";
-		}
-		else {
-
-			cout << last << '\n';
-		}
+		last = stoi(a) + 3;
 	}
 	else if (b != "Fizz" && b != "Buzz" && b != "FizzBuzz") {
-		int last = stoi(b) + 2;
-		if (last % 15 == 0) {
-			cout << "FizzBuzz" << "\n";
-		}
-		else if (last % 5 == 0) {
-			cout << "Buzz" << "\n";
-		}
-		else if (last % 3 == 0) {
-			cout << "Fizz" << "\n";
-		}
-		else {
-
-			cout << last << '\n';
-		}
+		last = stoi(b) + 2;
 	}
 	else if (c != "Fizz" && c != "Buzz" && c != "FizzBuzz") {
-		int last = stoi(c) + 1;
-		if (last % 15 == 0) {
-			cout << "FizzBuzz" << "\n";
-		}
-		else if (last % 5 == 0) {
-			cout << "Buzz" << "\n";
-		}
-		else if (last % 3 == 0) {
-			cout << "Fizz" << "\n";
-		}
-		else {
-
-			cout << last << '\n';
-		}
+		last = stoi(c) + 1;
+	}
+	if (last % 15 == 0) {
+		cout << "FizzBuzz" << "\n";
+	}
+	else if (last % 5 == 0) {
+		cout << "Buzz" << "\n";
+	}
+	else if (last % 3 == 0) {
+		cout << "Fizz" << "\n";
+	}
+	else {
+		cout << last << '\n';
 	}
 	return 0;
 }
