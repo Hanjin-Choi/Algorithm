74b6dd4bb1a2de7f3184aef0320709828e8719d9
HanJin Choi <156498561+halfoffaceeyes@users.noreply.github.com>
2024-06-29 12:05:20 +0900

    [Gold II] Title: 보석 도둑, Time: 528 ms, Memory: 201164 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -1,20 +1,29 @@
 import sys
-input = sys.stdin.readline
-from heapq import heappush,heappop
+from heapq import heappush, heappop
 
-n,k = map(int,input().split())
-jewel =[list(map(int,input().split())) for _ in range(n)]
-bag = [int(input()) for _ in range(k)]
-jewel.sort()
-bag.sort()
-pq =[]
-idx=0
-ans=0
-for b in bag:
-    while idx<n and jewel[idx][0]<= b:
-        heappush(pq,-jewel[idx][1])
-        idx+=1
-    if pq:
-        v=heappop(pq)
-        ans -=v
-print(ans)
+def main():
+    input = sys.stdin.readline
+    N, K = map(int, input().split())
+    jewels = dict()
+    for _ in range(N):  # 반복문 N번을 도는 것은 동일하지만, list comprehesion에 비해 느릴 수도 있음
+        weight, value = map(int, input().split())
+        if weight in jewels:
+            jewels[weight].append(value)
+        else:
+            jewels[weight] = [value]
+    jewels_weight = sorted(jewels.keys(), reverse=True)  # 가방의 개수를 줄였으므로 여기서 nlogn의 시간복잡도가 조금 줄었을 수 있음
+        
+    bags = sorted([int(input()) for _ in range(K)])
+
+    result = 0
+    heap = []
+    for bag in bags:
+        while jewels_weight and jewels_weight[-1] <= bag:  # 어차피 도는 반복문의 횟수는 똑같음
+            weight = jewels_weight.pop()
+            for value in jewels[weight]:  # 앞서 아낀 부분도 여기서 추가로 돌기 때문
+                heappush(heap, -value)
+        if heap:
+            result += heappop(heap)
+    print(-result)
+# 그런데 왜 시간이 훨씬 빠른가..?
+main()
