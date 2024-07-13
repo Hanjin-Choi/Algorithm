683dc4f8f4c9dbc203d0a0102ab475e4b327ca75
HanJin Choi <156498561+halfoffaceeyes@users.noreply.github.com>
2024-07-13 18:39:40 +0900

    [Gold V] Title: 평범한 배낭, Time: 3620 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -3,13 +3,8 @@ input = sys.stdin.readline
 
 n,k = map(int,input().split())
 bag = [list(map(int,input().split())) for _ in range(n)]
-arr= [[0]*(k+1) for _ in range(n)]
+arr=[0]*(k+1)
 for idx in range(n):
-    for weight in range(1,k+1):
-        if idx==0 and weight >=bag[idx][0]:
-            arr[0][weight]=bag[0][1]
-        elif weight >=bag[idx][0]:
-            arr[idx][weight]=max(arr[idx-1][weight],arr[idx-1][weight-bag[idx][0]]+bag[idx][1])
-        else:
-            arr[idx][weight]=arr[idx-1][weight]
-print(arr[n-1][weight])
+    for weight in range(k,bag[idx][0]-1,-1):
+        arr[weight] =max(arr[weight-bag[idx][0]]+bag[idx][1], arr[weight])
+print(arr[k])
