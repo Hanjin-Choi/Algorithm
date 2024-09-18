9561dad4dcb16f8b47f320f1171be5c793eadc18
HanJin Choi <156498561+Hanjin-Choi@users.noreply.github.com>
2024-09-19 04:05:47 +0900

    [Bronze III] Title: 별 찍기 - 15, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int n;
+string a;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 0; j < n - i; j++)
+			cout << ' ';
+		cout << '*';
+		if (i != 1)
+		{
+			for (int j = 0; j < 2* i-3; j++)
+				cout << ' ';
+			cout << '*';
+		}
+		cout << endl;
+	}
+	return 0;
+}
